bc0fbbe73a575e36a1661dc362e02c7e2cfbbf12
ibeandyy <adhyr4@gmail.com>
2023-09-22 22:48:14 -0700

    added logger

--- cmd/main.go
+++ cmd/main.go
@@ -14,13 +14,29 @@ import (
 var (
 	path   = flag.String("path", "D:/Infogenesis/Log_dir/log_pms", "relative path to directory")
 	outDir = flag.String("out", "D:/Infogenesis/Log_dir/log_pms/BinBackup", "relative path to output directory")
+	logDir = flag.String("log", "D:/Infogenesis/Log_dir/log_pms/", "relative path to log directory")
 )
 
 const maxGoroutines = 100 // Adjust as needed
 
-func main() {
-	flag.Parse()
+var logger *log.Logger
 
+func init() {
+	flag.Parse()
+	initLogger()
+	logger.Println("Program started at:", time.Now().Format(time.RFC3339))
+}
+
+func initLogger() {
+	logFileName := "BinMover" + time.Now().Format("2006-01-02") + ".log"
+	logFile, err := os.OpenFile(filepath.Join(*logDir, logFileName), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %v", err)
+	}
+	logger = log.New(logFile, "", log.LstdFlags)
+}
+
+func main() {
 	// Construct the bin backup directory with the current date
 	binBackupDir := filepath.Join(*outDir, time.Now().Format("2006-01-02"))
 
@@ -35,6 +51,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	binCount := 0
+	for _, entry := range entries {
+		if strings.HasSuffix(strings.ToUpper(entry.Name()), ".BIN") {
+			binCount++
+		}
+	}
+	logger.Printf("Number of .BIN files in the directory: %d", binCount)
 
 	sem := make(chan struct{}, maxGoroutines)
 	var wg sync.WaitGroup
@@ -68,6 +92,11 @@ func main() {
 	for err := range errCh {
 		log.Println("Error:", err)
 	}
+
+	movedCount := binCount - len(errCh)
+	logger.Printf("Number of moved .BIN files: %d", movedCount)
+
+	logger.Println("Program exited at:", time.Now().Format(time.RFC3339))
 }
 
 func moveFile(srcPath, destDir string) error {
